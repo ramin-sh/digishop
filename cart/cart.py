d9cfd110cfc5f4251feb1e96604bb2c4ad000a1e
ramin-sh <ramin.a.sh2012@gmail.com>
2024-01-26 19:57:05 +0330

    ezafe kardan tedad be sabad kharid

--- cart/cart.py
+++ cart/cart.py
@@ -8,14 +8,15 @@ class Cart:
             cart = self.session['session_key'] = {}
         self.cart = cart
 
-    def add(self,product):
+    def add(self,product,quantity):
       
         product_id = str(product.id)
+        product_qty = str(quantity)
 
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price':str(product.price)}
+            self.cart[product_id] = int(product_qty)
 
         self.session.modified = True
 
@@ -27,3 +28,7 @@ class Cart:
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in = product_ids)
         return products 
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
--- cart/views.py
+++ cart/views.py
@@ -7,15 +7,17 @@ from .cart import Cart
 def cart_summary(request):
     cart = Cart(request)
     cart_products =  cart.get_prods()
-    return render(request,'cart_summary.html',{'cart_products':cart_products})
+    quantities = cart.get_quants()
+    return render(request,'cart_summary.html',{'cart_products':cart_products,'quantities':quantities})
 
 def cart_add(request):
     cart = Cart(request)
     
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(Product,id =product_id)
-        cart.add(product=product)
+        cart.add(product=product, quantity = product_qty)
         cart_quantitiy = cart.__len__()
         
         # response = JsonResponse({'Product name':product.name})
